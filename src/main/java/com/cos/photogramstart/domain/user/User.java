0e7f733a882fb75edaf8144a494f37fc2c4130d0
김동진 <dongjin403@naver.com>
2022-08-29 19:59:39 +0900

    회원가입 진행(암호화, DB중복값 처리)

---
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -0,0 +1,47 @@
+package com.cos.photogramstart.domain.user;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@Column(unique = true)
+	private String username;
+	private String password;
+	
+	private String name;
+	private String website;
+	private String bio;
+	private String email;
+	private String phone;
+	private String gender; 
+	
+	private String profileImageUrl; //사진
+	private String role; //권한
+	
+	private LocalDateTime creaDateTime;
+	
+	@PrePersist //DB에 insert되기전에 실행
+	public void createDate() {
+		this.creaDateTime = LocalDateTime.now(); 
+	}
+	
+}
