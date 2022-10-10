08facbf085ae4758349c5401c3b01ac7ea6b3cf7
김동진 <dongjin403@naver.com>
2022-10-10 13:58:19 +0900

    인기페이지 구성(쿼리중요!!) mPopular 쿼리보기

--- src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
+++ src/main/java/com/cos/photogramstart/domain/image/ImageRepository.java
@@ -7,8 +7,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface ImageRepository extends JpaRepository<Image, Integer>{
-	
-	@Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fromUserId = :principalId) ORDER BY id DESC",nativeQuery = true)
+public interface ImageRepository extends JpaRepository<Image, Integer> {
+
+	@Query(value = "SELECT * FROM image WHERE userId IN (SELECT toUserId FROM subscribe WHERE fromUserId = :principalId) ORDER BY id DESC", nativeQuery = true)
 	Page<Image> mStory(int principalId, Pageable pageable);
+
+	@Query(value = "SELECT i.* FROM image i JOIN (SELECT imageId, COUNT(imageId) likeCount FROM likes GROUP BY imageId) c on i.id = c.imageId ORDER BY likeCount desc" ,nativeQuery = true)
+	List<Image> mPopular();
 }
